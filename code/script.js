4a1367ff177d4d24005a7f58bba5e673431366f6
Jonna Karlsson <jonna@tassen.se>
2026-03-19 09:45:06 +0100

    Step 1 and 2, asked for name and type of Food

--- code/script.js
+++ code/script.js
@@ -1,13 +1,33 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
+
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  `Welcome to our Italian cuisine. Ready to order? - Click 'OK' to begin.`
 )
+let firstname = prompt(`Please enter your first name`); 
+
+alert(`Hi ${firstname}`);
 
 // Step 2 - Food choice
-// Your code goes here
+
+let typeChoice = prompt(
+  `Please select a number to choose your choice of food: \n1 = Pizza\n2 = Pasta\n3 = Sallad`
+);
+
+let foodType; 
+  if (typeChoice === "1"){
+    foodType = "pizza";
+  } 
+  else if (typeChoice === "2"){
+    foodType = "pasta"; 
+  }
+  else if (typeChoice === "3"){
+    foodType = "sallad";
+  }
+  else {
+      alert (`You need to pick a valid number 1,2 or 3. Bot will close, please refresh page to start over.`);
+    }
 
 // Step 3 - Subtype choice
 // Your code goes here
