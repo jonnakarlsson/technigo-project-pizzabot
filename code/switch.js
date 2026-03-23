1408d4f79dd7ee6a5ca0f13e71a021c4ef356f08
Jonna Karlsson <jonna@tassen.se>
2026-03-23 13:50:40 +0100

    added switch file

---
+++ code/switch.js
@@ -0,0 +1,145 @@
+// Start here
+
+// Step 1 - Welcome and introduction
+
+alert(
+  `Welcome to our Switch Italian cuisine. Ready to order? - Click 'OK' to begin.`,
+);
+let firstname = prompt(`Please enter your first name`);
+
+alert(`Hi ${firstname}`);
+
+// Step 2 - Food choice
+
+let typeChoice = prompt(
+  `Please select a number to choose your choice of food: 
+  1 = Pizza 
+  2 = Pasta
+  3 = Salad`,
+);
+
+let foodType;
+switch (typeChoice) {
+  case "1":
+    foodType = "pizza";
+    break;
+  case "2":
+    foodType = "pasta";
+    break;
+  case "3":
+    foodType = "salad";
+    break;
+  default:
+    alert(`This was an invalid choice`);
+}
+alert(`You chose ${foodType}, good choice!`);
+
+// Step 3 - Subtype choice
+
+let finalDish;
+
+if (foodType === "pizza") {
+  let pizzaChoice = prompt(
+    `Please select a number to choose your choice of pizza: 
+    1 = Feta Cheese Pizza
+    2 = Calzone
+    3 = Bot-Spetzial Pizza`,
+  );
+  switch (pizzaChoice) {
+    case "1":
+      finalDish = "Feta Cheese Pizza";
+      break;
+    case "2":
+      finalDish = "Calzone";
+      break;
+    case "3":
+      finalDish = "Bot-Spetzial Pizza";
+      break;
+    default:
+      alert(`This was an invalid choice`);
+  }
+} else if (foodType === "pasta") {
+  let pastaChoice = prompt(
+    `Please select a number to choose your choice of Pasta: 
+    1 = Carbonara
+    2 = Bolognese
+    3 = Bot-Spetzial Pasta`,
+  );
+  switch (pastaChoice) {
+    case "1":
+      finalDish = "Carbonara";
+      break;
+    case "2":
+      finalDish = "Bolognese";
+      break;
+    case "3":
+      finalDish = "Bot-Spetzial Pasta";
+      break;
+    default:
+      alert(`This was an invalid choice`);
+  }
+} else if (foodType === "salad") {
+  let saladChoice = prompt(
+    `Please select a number to choose your choice of Salad: 
+    1 = Ceasar salad
+    2 = Goat Cheese salad
+    3 = Bot-Spetzial Salad`,
+  );
+  switch (saladChoice) {
+    case "1":
+      finalDish = "Ceasar salad";
+      break;
+    case "2":
+      finalDish = "Goat Cheese salad";
+      break;
+    case "3":
+      finalDish = "Bot-Spetzial Salad";
+      break;
+    default:
+      alert(`This is no salad`);
+  }
+}
+alert(`You picked ${finalDish}`);
+
+// Step 4 - Age
+let age = prompt(`Please enter your age`);
+let adult;
+if (age >= 13) {
+  adult = true;
+}
+let price;
+if (adult) {
+  price = 15;
+} else {
+  price = 9;
+}
+alert(`Your price is ${price} EUR`);
+
+// Step 5 - Order confirfinalDishion
+
+let order;
+
+if (adult) {
+  let confirmation = prompt(
+    `You selected a ${finalDish}, and the price will be ${price}. 
+    Select a number to confirm:
+  1 = I confirm my order 
+  2 = I changed my mind`,
+  );
+  if (confirmation === "1") order = true;
+  else if (confirmation === "2") order = false;
+  else alert(`Invalid choice!`);
+} else {
+  let confirmation = prompt(
+    `You selected a childsize ${finalDish}, and the price will be ${price}. 
+    Select a number to confirm:
+  1 = I confirm my order 
+  2 = I changed my mind`,
+  );
+  if (confirmation === "1") order = true;
+  else if (confirmation === "2") order = false;
+  else alert(`Invadid choice!`);
+}
+if (order) {
+  alert(`Thank you for your order! It will be ready for pick-up in 10 minutes`);
+}
